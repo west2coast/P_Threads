1d08fd53c8225c157298f374222c286d7b75439e
west2coast <User@Chriss-MacBook-Pro-2.local>
2016-10-01 12:51:02 -0400

    Decoder broken into parts works

---
+++ Decode/Hashemi_decode_logic/Hashemi_decode_logic/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hashemi_decode_logic
+//
+//  Created by Chris West on 10/1/16.
+//  Copyright © 2016 Chris West. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
