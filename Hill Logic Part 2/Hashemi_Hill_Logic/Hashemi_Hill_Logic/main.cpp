df38d7ad67c830bdcf2e9ed28b56c7949828d3c1
west2coast <User@Chriss-MacBook-Pro-2.local>
2016-10-02 01:38:16 -0400

    Starting part 2

---
+++ Hill Logic Part 2/Hashemi_Hill_Logic/Hashemi_Hill_Logic/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hashemi_Hill_Logic
+//
+//  Created by Chris West on 10/1/16.
+//  Copyright © 2016 Chris West. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
